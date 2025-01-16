cc5b078abb9bb31e08142c55e6d6697765917039
Barry Halper <69764062+barryhalper@users.noreply.github.com>
2025-01-16 10:03:38 +0000

    updates page layout with new title

--- spec/acceptance/layout_spec.rb
+++ spec/acceptance/layout_spec.rb
@@ -1,21 +1,24 @@
 # frozen_string_literal: true
 
-describe "Acceptance::AccessibilityStatement", type: :feature do
+describe "Acceptance::Layout", type: :feature do
   include RSpecFrontendServiceMixin
 
+  let(:local_host){
+    "http://find-energy-performance-data"
+  }
 
   describe "get .find-energy-certificate-data.epb-frontend" do
     context "when the home page is rendered" do
-      let(:response) { get "http://epc-data.local.gov.uk" }
+      let(:response) { get local_host }
 
       it "tab value is the same as the main header value" do
         expect(response.body).to include(
-          "<title>Find an energy certificate – Find an energy certificate – GOV.UK</title>",
+          "<title>Find energy performance of buildings data – GOV.UK</title>",
         )
       end
 
       it "includes the gov header" do
-        expect(response.body).to have_link "Find an energy certificate"
+        expect(response.body).to have_link "Find energy performance of buildings data"
       end
 
       it "does not allow indexing or following by crawlers" do
@@ -24,11 +27,5 @@ describe "Acceptance::AccessibilityStatement", type: :feature do
     end
   end
 
-  describe "non start pages" do
-    let(:response) { get "http://find-energy-certificate.local.gov.uk/find-a-certificate/type-of-property" }
 
-    it "does not allow indexing or following by crawlers" do
-      expect(response.body).to include("<meta name=\"robots\" content=\"noindex, nofollow\">")
-    end
-  end
 end
