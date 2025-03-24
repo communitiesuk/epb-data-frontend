db6b3fe2ca919f0933d75e2ea552f0136a3ca7e3
Aleksandra Ivanova <aleksandra98iv@gmail.com>
2025-03-24 16:08:07 +0000

    OD-78: Adds filter properties journey tests

---
+++ spec/journey/filter_properties_spec.rb
@@ -0,0 +1,52 @@
+# frozen_string_literal: true
+
+describe "Journey::FilterProperties", :journey, type: :feature do
+  let(:getting_domain) do
+    "http://get-energy-performance-data.epb-frontend:9393"
+  end
+
+  process_id = nil
+
+  before(:all) do
+    process =
+      IO.popen(
+        [
+          "rackup",
+          "config_test.ru",
+          "-q",
+          "-o",
+          "127.0.0.1",
+          "-p",
+          "9393",
+          { err: %i[child out] },
+        ],
+      )
+    process_id = process.pid
+
+    nil unless process.readline.include?("port=9393")
+  end
+
+  after(:all) { Process.kill("KILL", process_id) if process_id }
+
+  context "when downloading all data" do
+    before do
+      visit "#{getting_domain}/filter-properties?property_type=domestic"
+      click_on "Download all"
+    end
+
+    it "shows the expected destination page content" do
+      expect(page).to have_link("Start now", href: "/data-access-options")
+    end
+  end
+
+  context "when downloading default filtered data" do
+    before do
+      visit "#{getting_domain}/filter-properties?property_type=domestic"
+      click_on "Download selected"
+    end
+
+    it "shows the expected destination page content" do
+      expect(page).to have_link("Start now", href: "/data-access-options")
+    end
+  end
+end
