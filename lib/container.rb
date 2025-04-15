de48f808b16d46a4a031468ee422fea1fa9843a0
Pedro Alvarez <pedro.alvarez@madetech.com>
2025-04-15 11:49:22 +0100

    OD-109: Fix api initialisation

--- lib/container.rb
+++ lib/container.rb
@@ -4,12 +4,18 @@ require "aws-sdk-sns"
 
 class Container
   def initialize
+    internal_api_client = Auth::HttpClient.new ENV["EPB_AUTH_CLIENT_ID"],
+                                               ENV["EPB_AUTH_CLIENT_SECRET"],
+                                               ENV["EPB_AUTH_SERVER"],
+                                               ENV["EPB_DATA_WAREHOUSE_API_URL"],
+                                               OAuth2::Client,
+                                               faraday_connection_opts: { request: { timeout: 8 } }
+
     sns_gateway = Gateway::SnsGateway.new(sns_client)
     send_download_request_use_case = UseCase::SendDownloadRequest.new(sns_gateway:)
-    certificate_count_gateway = Gateway::CertificateCountGateway.new(api_client:)
+    certificate_count_gateway = Gateway::CertificateCountGateway.new(internal_api_client)
     get_download_size_use_case = UseCase::GetDownloadSize.new(certificate_count_gateway:)
     @objects = {
-      internal_api_client: api_client,
       send_download_request_use_case:,
       get_download_size_use_case:,
     }
@@ -20,15 +26,6 @@ class Container
   end
 
 private
-
-  def api_client
-    Auth::HttpClient.new ENV["EPB_AUTH_CLIENT_ID"],
-                         ENV["EPB_AUTH_CLIENT_SECRET"],
-                         ENV["EPB_AUTH_SERVER"],
-                         ENV["EPB_DATA_WAREHOUSE_API_URL"],
-                         OAuth2::Client,
-                         faraday_connection_opts: { request: { timeout: 8 } }
-  end
 
   def sns_client
     aws_credentials = ENV["STAGE"] == "test" ? Aws::Credentials.new(ENV["AWS_TEST_ACCESS_ID"], ENV["AWS_TEST_ACCESS_KEY"]) : Aws::ECSCredentials.new
