d0f30b0a6fd98283ed0ebf571fefc83881fa4af6
Pedro Alvarez <pedro.alvarez@madetech.com>
2025-04-16 11:19:31 +0100

    OD-109: Adds /api-healthcheck endpoint to test api connectivity

--- lib/controller/home_controller.rb
+++ lib/controller/home_controller.rb
@@ -17,5 +17,23 @@ module Controller
       @hide_guidance_text = true
       erb :guidance
     end
+
+    get "/api-healthcheck" do
+      uri = URI("#{ENV['EPB_DATA_WAREHOUSE_API_URL']}/healthcheck")
+      response = Net::HTTP.get_response(uri)
+
+      status response.code.to_i
+      {
+        status: response.code.to_i,
+        message: "Connection to API: OK",
+      }.to_json
+    rescue StandardError => e
+      status 500
+      {
+        status: 500,
+        error: "Failed to reach API",
+        details: e.message,
+      }.to_json
+    end
   end
 end
