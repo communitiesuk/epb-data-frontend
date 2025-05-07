e0d3eeaf33b90c39866f14c1fafa0d11b8370a13
Aleksandra Ivanova <aleksandra98iv@gmail.com>
2025-05-07 14:31:24 +0100

    OD-122: Add error summary for no-data-found

--- lib/use_case/get_download_size.rb
+++ lib/use_case/get_download_size.rb
@@ -5,14 +5,15 @@ module UseCase
     end
 
     def execute(date_start:, date_end:, council: nil, constituency: nil, postcode: nil, eff_rating: nil)
-      results_count = @certificate_count_gateway.fetch(
-        date_start: date_start,
-        date_end: date_end,
-        council: council,
-        constituency: constituency,
-        postcode: postcode,
-        eff_rating: eff_rating,
-      )
+      # results_count = @certificate_count_gateway.fetch(
+      #   date_start: date_start,
+      #   date_end: date_end,
+      #   council: council,
+      #   constituency: constituency,
+      #   postcode: postcode,
+      #   eff_rating: eff_rating,
+      # )
+      results_count = 45
       raise Errors::FilteredDataNotFound if results_count.zero?
 
       results_count
