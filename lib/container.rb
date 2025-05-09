0d75960e26629cf7eef22581fb9fc86db7480a32
Pedro Alvarez <pedro.alvarez@madetech.com>
2025-05-09 10:51:49 +0100

    OD-131: Increase HttpClient timeout to 20
    We only use this client to call the count endpoint,
    if we use it for anything else we should create another
    client with a lower timeout.

--- lib/container.rb
+++ lib/container.rb
@@ -5,16 +5,16 @@ require "aws-sdk-s3"
 
 class Container
   def initialize
-    internal_api_client = Auth::HttpClient.new ENV["EPB_AUTH_CLIENT_ID"],
-                                               ENV["EPB_AUTH_CLIENT_SECRET"],
-                                               ENV["EPB_AUTH_SERVER"],
-                                               ENV["EPB_DATA_WAREHOUSE_API_URL"],
-                                               OAuth2::Client,
-                                               faraday_connection_opts: { request: { timeout: 8 } }
+    count_api_client = Auth::HttpClient.new ENV["EPB_AUTH_CLIENT_ID"],
+                                            ENV["EPB_AUTH_CLIENT_SECRET"],
+                                            ENV["EPB_AUTH_SERVER"],
+                                            ENV["EPB_DATA_WAREHOUSE_API_URL"],
+                                            OAuth2::Client,
+                                            faraday_connection_opts: { request: { timeout: 20 } }
 
     sns_gateway = Gateway::SnsGateway.new
     send_download_request_use_case = UseCase::SendDownloadRequest.new(sns_gateway:, topic_arn: ENV["SEND_DOWNLOAD_TOPIC_ARN"])
-    certificate_count_gateway = Gateway::CertificateCountGateway.new(internal_api_client)
+    certificate_count_gateway = Gateway::CertificateCountGateway.new(count_api_client)
     get_download_size_use_case = UseCase::GetDownloadSize.new(certificate_count_gateway:)
     get_presigned_url_use_case = UseCase::GetPresignedUrl.new(gateway: Gateway::S3Gateway.new, bucket_name: ENV["AWS_S3_USER_DATA_BUCKET_NAME"])
     @objects = {
