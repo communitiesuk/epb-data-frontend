4d26a3f31f5011c4702c194df982da46dd5b5099
Aleksandra Ivanova <aleksandra98iv@gmail.com>
2025-05-28 13:31:57 +0300

    Removes unused code from the sns gateway test

--- spec/unit/gateway/sns_gateway_spec.rb
+++ spec/unit/gateway/sns_gateway_spec.rb
@@ -3,12 +3,6 @@ describe Gateway::SnsGateway do
 
   let(:topic_arn) { "arn:aws:sns:eu-west-2:123456789:MySNSTopic" }
 
-  let(:url) { "https://sns.eu-west-2.amazonaws.com/" }
-
-  let(:headers) do
-    { "Content-Type" => "application/x-www-form-urlencoded; charset=utf-8" }
-  end
-
   let(:message) do
     {
       "email_address" => "test@example.com",
@@ -23,30 +17,6 @@ describe Gateway::SnsGateway do
   end
 
   context "when the message is successful" do
-    let(:mock_body_response) do
-      <<~XML
-        <PublishResponse xmlns="https://sns.amazonaws.com/doc/2010-03-31/">
-          <PublishResult>
-            <MessageId>messageId</MessageId>
-          </PublishResult>
-        </PublishResponse>
-      XML
-    end
-
-    # before do
-    #   WebMock.stub_request(:post, url)
-    #          .with(
-    #            headers: headers,
-    #            body: {
-    #              "Action" => "Publish",
-    #              "TopicArn" => topic_arn,
-    #              "Message" => message.to_json,
-    #              "Version" => "2010-03-31",
-    #            },
-    #          )
-    #          .to_return(status: 200, body: mock_body_response, headers: { "Content-Type" => "text/xml" })
-    # end
-
     it "sends the message to the sns topic" do
       response = gateway.send_message(topic_arn, message)
       expect(response.message_id).to eq("messageId")
