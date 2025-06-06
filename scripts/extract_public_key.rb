c6da72411c4b47cf06abe0fd7d7b09730243dede
Pedro Alvarez <pedro.alvarez@madetech.com>
2025-06-06 11:40:15 +0100

    Revert "Improves One Login Simulator script"
    This reverts commit 3f71c8b81339319698ad5760ad5abcac1044be8b.

--- scripts/extract_public_key.rb
+++
@@ -1,4 +0,0 @@
-require "json"
-require_relative "../spec/test_doubles/one_login_stub"
-
-puts JSON.parse(OneLoginStub.tls_keys)["public_key"]
--- scripts/one_login_simulator.sh
+++ scripts/one_login_simulator.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
 
-PUBLIC_KEY=$(ruby extract_public_key.rb)
-
 docker stop one-login-simulator
 
 docker run -e SIMULATOR_URL='http://localhost:3333' --rm --detach \
     -e PORT=3333 \
+    -e PUBLIC_KEY="-----BEGIN PUBLIC KEY----- MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEArGvxQU80uOxKzlmbCHNOkjcgTF415lSHTmbr3x8jFEHvXu+NzXD0qHMIIQ217foYJAwT/RdTpaaeOW7sXdIqgAUhQwCNhSyuTx0sIMM0G0YXHOmLXAiRzwApLBxKYYU4i66T6ACP7Io0pDEqHu0sFrfdnfV+3JaaRlWkGXpKarwMtMAhzSdE5UGgxJ08d7qLJ/g8lbQZxcrVmyLragmYHEfzgAYyv8WFKEj2n0rcFzntcjZXy9EZOxlFqMn27Vr/lz+Yye2zio4+j/d8S8Q6V1oddVHwMAB8rG+CaTJg+63Z61dtStYMxIl2CFBld4UpWTkWrGdmHnKkYZeZRnrm7QIDAQAB -----END PUBLIC KEY-----" \
     -e SCOPES="openid,email" \
-    -e CLIENT_ID="test.onelogin.client.id" \
-    -e PUBLIC_KEY="$PUBLIC_KEY" \
     -e REDIRECT_URLS="http://127.0.0.1:9292/login/callback" \
     --name one-login-simulator -p 3333:3333 ghcr.io/govuk-one-login/simulator:latest
 
