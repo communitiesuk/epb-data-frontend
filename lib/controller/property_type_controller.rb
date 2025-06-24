cbd585f7aaae1ecb6c1c62148584ff5d8aa35f26
Aleksandra Ivanova <aleksandra98iv@gmail.com>
2025-06-24 10:13:42 +0100

    OD-181: Alter the redirect in PropertyTypeController to debug

--- lib/controller/property_type_controller.rb
+++ lib/controller/property_type_controller.rb
@@ -9,7 +9,7 @@ module Controller
       case e
       when Errors::AuthenticationError
         logger.warn "Authentication error on type of properties controller: #{e.message}"
-        redirect "/login"
+        redirect "/"
       else
         server_error(e)
       end
