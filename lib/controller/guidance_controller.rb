6e7deabd0c0a0e9dd677d5f73db479153492bd1f
Aleksandra Ivanova <aleksandra98iv@gmail.com>
2025-11-06 13:25:28 +0000

    OD-295: Adds /how-to-link-certificates-to-recommendations endpoint
    - Added to provide information on how to link certificates to recommendations through guidance page

--- lib/controller/guidance_controller.rb
+++ lib/controller/guidance_controller.rb
@@ -6,5 +6,12 @@ module Controller
       @hide_guidance_text = true
       erb :guidance
     end
+
+    get "/how-to-link-certificates-to-recommendations" do
+      status 200
+      @back_link_href = request.referer || "/"
+      @hide_guidance_text = true
+      erb :'guidance_pages/link_certificates_to_recommendations'
+    end
   end
 end
--- spec/acceptance/guidance_view_spec.rb
+++ spec/acceptance/guidance_view_spec.rb
@@ -1,12 +1,11 @@
 describe "Acceptance::ServiceStartPage", type: :feature do
   include RSpecFrontendServiceMixin
-  let(:local_host) do
-    "http://get-energy-performance-data/guidance"
-  end
-
-  let(:response) { get local_host }
+  let(:base_url) { "http://get-energy-performance-data" }
 
   describe "get .get-energy-certificate-data.epb-frontend/guidance" do
+    let(:path) { "/guidance" }
+    let(:response) { get "#{base_url}#{path}" }
+
     context "when the start page is rendered" do
       it "returns status 200" do
         expect(response.status).to eq(200)
@@ -58,6 +57,51 @@ describe "Acceptance::ServiceStartPage", type: :feature do
 
       it "has the correct MHCLG contact email" do
         expect(response.body).to have_content("mhclg.digital-services@communities.gov.uk")
+      end
+
+      it "does not render content for guidance under the Get Help section" do
+        expect(response.body).not_to have_content("Visit the guidance page for information on:")
+      end
+    end
+  end
+
+  describe "get .get-energy-certificate-data.epb-frontend/how-to-link-certificates-to-recommendations" do
+    let(:path) { "/how-to-link-certificates-to-recommendations" }
+    let(:response) { get "#{base_url}#{path}" }
+
+    context "when the start page is rendered" do
+      it "returns status 200" do
+        expect(response.status).to eq(200)
+      end
+
+      it "shows a back link and redirects to previous page" do
+        header "Referer", "/previous_page"
+        expect(response.body).to have_link("Back", href: "/previous_page")
+      end
+
+      it "directs back link to home page if no referer header found" do
+        expect(response.body).to have_link("Back", href: "/")
+      end
+
+      it "has the correct title" do
+        expect(response.body).to have_css("h1", text: "How to link certificates to recommendations")
+      end
+
+      it "has the correct content under the title" do
+        expect(response.body).to have_css("p", text: "The method to link certificates to their recommendations varies depending on how the data is accessed:")
+      end
+
+      it "has the correct content for CSV download section" do
+        expect(response.body).to have_css("p", text: "For domestic EPC and commercial EPC data, the certificate numbers can be used to identify certificates and to link them with their recommendations.")
+      end
+
+      it "has the correct content for API section" do
+        expect(response.body).to have_css("p", text: "For domestic EPC data, the recommendation reports are included in the EPCs.")
+        expect(response.body).to have_css("p", text: "For commercial EPC data, the certificate numbers can be used to fetch the recommendation reports.")
+      end
+
+      it "has the Get Help or Give Feedback section" do
+        expect(response.body).to have_css("h2", text: "Get help or give feedback")
       end
 
       it "does not render content for guidance under the Get Help section" do
