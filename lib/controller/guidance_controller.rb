b5d6f2433e8be9b7e4b6a45ad12053607657ce3a
Aleksandra Ivanova <aleksandra98iv@gmail.com>
2025-11-07 11:42:15 +0000

    OD-295: Adds /data-protection endpoint

--- lib/controller/guidance_controller.rb
+++ lib/controller/guidance_controller.rb
@@ -34,5 +34,12 @@ module Controller
       @hide_guidance_text = true
       erb :'guidance_pages/licensing_restrictions'
     end
+
+    get "/data-protection" do
+      status 200
+      @back_link_href = request.referer || "/"
+      @hide_guidance_text = true
+      erb :'guidance_pages/data_protection'
+    end
   end
 end
--- spec/acceptance/guidance_view_spec.rb
+++ spec/acceptance/guidance_view_spec.rb
@@ -1,4 +1,4 @@
-describe "Acceptance::ServiceStartPage", type: :feature do
+describe "Acceptance::GuidancePage", type: :feature do
   include RSpecFrontendServiceMixin
   let(:base_url) { "http://get-energy-performance-data" }
 
@@ -233,6 +233,54 @@ describe "Acceptance::ServiceStartPage", type: :feature do
         expect(response.body).to have_link("Further information about these exceptions can be found here", href: "https://www.gov.uk/guidance/exceptions-to-copyright")
         expect(response.body).to have_link("PricingLicensing@os.uk", href: "mailto:PricingLicensing@os.uk")
         expect(response.body).to have_link("address.management@royalmail.com", href: "mailto:address.management@royalmail.com")
+      end
+
+      it "has the Get Help or Give Feedback section" do
+        expect(response.body).to have_css("h2", text: "Get help or give feedback")
+      end
+
+      it "does not render content for guidance under the Get Help section" do
+        expect(response.body).not_to have_content("Visit the guidance page for information on:")
+      end
+    end
+  end
+
+  describe "get .get-energy-certificate-data.epb-frontend/data-protection" do
+    let(:path) { "/data-protection" }
+    let(:response) { get "#{base_url}#{path}" }
+
+    context "when the start page is rendered" do
+      it "returns status 200" do
+        expect(response.status).to eq(200)
+      end
+
+      it "shows a back link and redirects to previous page" do
+        header "Referer", "/previous_page"
+        expect(response.body).to have_link("Back", href: "/previous_page")
+      end
+
+      it "directs back link to home page if no referer header found" do
+        expect(response.body).to have_link("Back", href: "/")
+      end
+
+      it "has the correct title" do
+        expect(response.body).to have_css("h1", text: "Data protection")
+      end
+
+      it "has the correct content for registration section" do
+        expect(response.body).to have_css("h2", text: "Registration")
+        expect(response.body).to have_css("p", text: "In order to adhere to the conditions of the Data Protection Act 2018 and the General Data Protection
+          Regulation, MHCLG retains the email address of those who access the data.")
+      end
+
+      it "has the correct content for data protection act section" do
+        expect(response.body).to have_css("h2", text: "Data Protection Act 2018")
+        expect(response.body).to have_link("Information Commissioner's Office", href: "https://ico.org.uk/")
+      end
+
+      it "has the correct content for personal data misuse section" do
+        expect(response.body).to have_css("h2", text: "How to report misuse of personal data")
+        expect(response.body).to have_link("Information Commissioner’s Office (ICO)", href: "https://ico.org.uk/")
       end
 
       it "has the Get Help or Give Feedback section" do
