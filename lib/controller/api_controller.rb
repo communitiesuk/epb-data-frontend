d0431affe749cd2bf398edd567db25dc441f930e
Aleksandra Ivanova <aleksandra98iv@gmail.com>
2025-11-11 13:09:48 +0000

    OD-295: Adds /api/energy-certificate-data-apis

--- lib/controller/api_controller.rb
+++ lib/controller/api_controller.rb
@@ -22,7 +22,17 @@ module Controller
 
       erb :api_guidance
     rescue StandardError => e
-      logger.error "Unexpected error during /api/my-account get endpoint: #{e.message}"
+      logger.error "Unexpected error during /api/api-guidance get endpoint: #{e.message}"
+      server_error(e)
+    end
+
+    get "/api/energy-certificate-data-apis" do
+      status 200
+      @back_link_href = request.referer || "/"
+
+      erb :energy_certificate_data_apis, locals: { use_case: @container.get_object(:get_user_token_use_case) }
+    rescue StandardError => e
+      logger.error "Unexpected error during /api/energy-certificate-data-apis get endpoint: #{e.message}"
       server_error(e)
     end
   end
