1e1bcf6a581651674998abad7333514a6f0836d5
Barry Halper <69764062+barryhalper@users.noreply.github.com>
2025-11-17 16:05:13 +0000

    OD-325 Api-tech docs refactored to separate end points
    Added code examples

--- lib/controller/api_tech_docs_controller.rb
+++ lib/controller/api_tech_docs_controller.rb
@@ -2,11 +2,31 @@ module Controller
   class ApiTechDocsController < Controller::BaseController
     get "/api-technical-documentation" do
       status 200
+      erb :'api_tech_docs/overview'
+    end
+
+    get "/api-technical-documentation/making-a-request" do
+      status 200
+      erb :'api_tech_docs/making_request'
+    end
+
+    get "/api-technical-documentation/headers" do
+      set_headers
+      erb :'api_tech_docs/headers'
+    end
+
+    get "/api-technical-documentation/fetch-certificate-data" do
+      set_headers
+      erb :'api_tech_docs/fetch_certificate_data'
+    end
+
+  private
+
+    def set_headers
       status 200
       @back_link_href = request.referer || "/"
       @hide_guidance_text = true
       @main_no_padding = true
-      erb :'api_tech_docs/overview'
     end
   end
 end
