b9a0e46bc8b74e27af4391047348a23211a1dc1f
Barry Halper <69764062+barryhalper@users.noreply.github.com>
2025-11-17 12:28:16 +0000

    OD-325 Adds fetch-certificates section
    Adds the rouge gem to render the JSON
    Uses the gem in the view model

--- lib/view_models/api_tech_docs.rb
+++ lib/view_models/api_tech_docs.rb
@@ -1,8 +1,16 @@
+require "rouge"
+
 module ViewModels
   class ApiTechDocs < ViewModels::FilterProperties
     def self.get_bearer_token(session, use_case)
       user_id = Helper::Session.get_session_value(session, :user_id)
       use_case.execute(user_id)
+    end
+
+    def self.markdown(text)
+      formatter = Rouge::Formatters::HTML.new
+      lexer = Rouge::Lexers::Shell.new
+      formatter.format(lexer.lex(text))
     end
   end
 end
