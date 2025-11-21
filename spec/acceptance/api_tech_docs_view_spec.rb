9a256425f8212c5788e5aeda11860304a094887e
Barry Halper <69764062+barryhalper@users.noreply.github.com>
2025-11-21 12:02:11 +0000

    OD-321 extends api-tech docs tests
    Updates some erroneous mark up found by extending the test
    Adds a shared context to be used for the navigation tests

--- spec/acceptance/api_tech_docs_view_spec.rb
+++ spec/acceptance/api_tech_docs_view_spec.rb
@@ -1,18 +1,82 @@
+require_relative "../shared_context/shared_api_tech_docs"
+
 describe "Acceptance::ApiTechnicalDocumentation", type: :feature do
   include RSpecFrontendServiceMixin
+
   let(:base_url) { "http://get-energy-performance-data" }
 
   describe "get .get-energy-certificate-data.epb-frontend/api-technical-documentation" do
+    include_context "when viewing api tech docs"
+
     let(:path) { "/api-technical-documentation" }
-    let(:response) { get "#{base_url}#{path}" }
 
     context "when the start page is rendered" do
+      let(:response) { get "#{base_url}#{path}" }
+
       it "returns status 200" do
         expect(response.status).to eq(200)
       end
 
       it "has the correct title" do
         expect(response.body).to have_css("h1", text: "Energy certificate data API documentation")
+      end
+
+      it "has the navigation component" do
+        expect(response.body).to have_css("nav.app-subnav")
+      end
+
+      it "the navigation has a link to each page" do
+        expect(response.body).to have_css("ul.app-subnav__section li", count: 16)
+      end
+
+      it "the navigation has a 2 sections" do
+        expect(response.body).to have_css("nav h3", text: "General documentation")
+        expect(response.body).to have_css("nav h3", text: "API specifications")
+      end
+
+      it "has the expected navigation links" do
+        page_urls.each do |link|
+          url = "#{path}/#{link}"
+          expect(response.body).to have_css("ul.app-subnav__section li a[href='#{url}']")
+        end
+      end
+    end
+
+    context "when requesting each api document" do
+      it "returns status 200 for each page" do
+        page_urls.each do |link|
+          response = get "#{base_url}#{path}/#{link}"
+          expect(response.status).to eq(200)
+        end
+      end
+
+      it "each page has the correct sections" do
+        page_urls.each do |link|
+          response = get "#{base_url}#{path}/#{link}"
+          expect(response.body).to have_css("ul.app-subnav__section")
+          expect(response.body).to have_css("h1")
+          expect(response.body).to have_css("h2")
+        end
+      end
+
+      context "when calling the pages that document endpoints" do
+        it "each page has the expected sections" do
+          end_points.each do |link|
+            response = get "#{base_url}#{path}/#{link}"
+            expect(response.body).to have_css("h2", text: "Method")
+            expect(response.body).to have_css("h2", text: "Response")
+            expect(response.body).to have_css("h2", text: "Example")
+          end
+        end
+
+        context "when calling the end point documents that have params" do
+          it "each page has the expected sections" do
+            end_points_params.each do |link|
+              response = get "#{base_url}#{path}/#{link}"
+              expect(response.body).to have_css("h2", text: "Parameters")
+            end
+          end
+        end
       end
     end
   end
---
+++ spec/shared_context/shared_api_tech_docs.rb
@@ -0,0 +1,25 @@
+shared_context "when viewing api tech docs" do
+  def page_urls
+    %w[ making-a-request
+        search-certificates/domestic
+        search-certificates/non-domestic
+        search-certificates-changed
+        download/domestic/csv
+        download-info/domestic/csv
+        download/domestic/json
+        download-info/domestic/json
+        download-info/non-domestic/csv
+        download/non-domestic/json
+        download-info/non-domestic/json
+        codes
+        codes-info]
+  end
+
+  def end_points
+    page_urls.excluding ["making-a-request"]
+  end
+
+  def end_points_params
+    page_urls.select { |i| i.start_with? "codes-", "search" }
+  end
+end
