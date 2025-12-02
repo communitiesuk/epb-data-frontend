c4711dc5e2eda5b8be05946a716c78fd533b2544
abdalwarsame <abdal.warsame@communities.gov.uk>
2025-12-02 15:53:44 +0000

    EPBR-9213 Created the opt_out page
    Added content to the start page and tested. Waiting on Welsh translations.

--- spec/acceptance/opt_out_home_view_spec.rb
+++ spec/acceptance/opt_out_home_view_spec.rb
@@ -9,5 +9,17 @@ describe "Acceptance::OptOutHome", type: :feature do
     it "returns status 200" do
       expect(response.status).to eq(200)
     end
+
+    it "displays the title as expected" do
+      expect(response.body).to have_css("h1", text: "Opting out an EPC")
+    end
+
+    it "has a link for viewing the guidance page" do
+      expect(response.body).to have_link("View the guidance", href: "https://www.gov.uk/guidance/energy-performance-certificates-opt-out-of-public-disclosure")
+    end
+
+    it "has a link to continue to opt_out reason page" do
+      expect(response.body).to have_link("Continue", href: "/opt-out/reason")
+    end
   end
 end
