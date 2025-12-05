ae8bd851ab68550ccfc06a25df08afce38e701d3
Pedro Alvarez <pedro.alvarez@madetech.com>
2025-12-05 15:18:23 +0000

    EPBR-9230: Add new callback url to one-login-simulator

--- scripts/one_login_simulator.sh
+++ scripts/one_login_simulator.sh
@@ -9,6 +9,6 @@ docker run -e SIMULATOR_URL='http://localhost:3333' --rm --detach \
     -e SCOPES="openid,email" \
     -e CLIENT_ID="test.onelogin.client.id" \
     -e PUBLIC_KEY="$PUBLIC_KEY" \
-    -e REDIRECT_URLS="http://127.0.0.1:9292/login/callback,http://127.0.0.1:9292/login/callback/admin"\
+    -e REDIRECT_URLS="http://127.0.0.1:9292/login/callback,http://127.0.0.1:9292/login/callback/admin,http://127.0.0.1:9292/login/callback/opt-out"\
     --name one-login-simulator -p 3333:3333 ghcr.io/govuk-one-login/simulator:latest
 
