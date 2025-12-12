57df0b459bdd65f68dc4aae70051d1f3fa333aed
Aleksandra Ivanova <aleksandra98iv@gmail.com>
2025-12-12 09:31:42 +0000

    OD-323: Fixes property_type validation to use non_domestic instead of non-domestic

--- lib/controller/filter_properties_controller.rb
+++ lib/controller/filter_properties_controller.rb
@@ -97,7 +97,7 @@ module Controller
     end
 
     def is_property_type_valid?(property_type)
-      %w[domestic non-domestic dec].include?(property_type)
+      %w[domestic non_domestic dec].include?(property_type)
     end
 
     def get_download_size(params_data)
