communitiesuk/epb-data-frontend

commit afac1063e890f36a2bb5e7da63e671c6d3a3b33e

EPBR-9238: Avoid running configure blocks multiple times

lib/controller/base_controller.rb
@@ -41,25 +41,6 @@ module Controller
     HOST_NAME = "get-energy-certificate-data".freeze
     Helper::Assets.setup_cache_control(self)
 
-    configure do
-      is_development = settings.environment == :development
-
-      use Rack::Session::Cookie,
-          key: "epb_data.session",
-          secret: ENV["SESSION_SECRET"],
-          expire_after: 60 * 60, # 1 hour
-          secure: !is_development,
-          same_site: is_development ? :lax : :none,
-          httponly: true
-    end
-
-    configure :development do
-      require "sinatra/reloader"
-      register Sinatra::Reloader
-      also_reload "lib/**/*.rb"
-      set :host_authorization, { permitted_hosts: [] }
-    end
-
     before do
       set_locale
       Helper::Session.set_local(session)

lib/frontend_service.rb
@@ -1,6 +1,25 @@
 # frozen_string_literal: true
 
 class FrontendService < Controller::BaseController
+  configure do
+    is_development = settings.environment == :development
+
+    use Rack::Session::Cookie,
+        key: "epb_data.session",
+        secret: ENV["SESSION_SECRET"],
+        expire_after: 60 * 60, # 1 hour
+        secure: !is_development,
+        same_site: is_development ? :lax : :none,
+        httponly: true
+  end
+
+  configure :development do
+    require "sinatra/reloader"
+    register Sinatra::Reloader
+    also_reload "lib/**/*.rb"
+    set :host_authorization, { permitted_hosts: [] }
+  end
+
   use Controller::HomeController
   use Controller::PropertyTypeController
   use Controller::FilterPropertiesController

commit 200937aa17a3db836e46db6854441a9fce9a8a85

EPBR-9238: Configure settings for development in base_controller
Dev tools broke after commit afac1063e890f36a2bb5e7da63e671c6d3a3b33e
returning 'Host not permitted' even for the home page. This commit
fixes it

lib/controller/base_controller.rb
@@ -60,6 +60,13 @@ module Controller
       redirect redirect_url
     end
 
+    configure :development do
+      require "sinatra/reloader"
+      register Sinatra::Reloader
+      also_reload "lib/**/*.rb"
+      set :host_authorization, { permitted_hosts: [] }
+    end
+
     def show(template, locals, layout = :layout)
       locals[:errors] = @errors
       erb template, layout:, locals:

lib/frontend_service.rb
@@ -18,12 +18,6 @@ class FrontendService < Controller::BaseController
     set :protection, except: [:path_traversal]
   end
 
-  configure :development do
-    require "sinatra/reloader"
-    register Sinatra::Reloader
-    also_reload "lib/**/*.rb"
-    set :host_authorization, { permitted_hosts: [] }
-  end
 
   use Controller::HomeController
 