e9650438dd5c7ce249f0f250dbb2ef913bc41599
abdalwarsame <abdal.warsame@communities.gov.uk>
2026-01-16 10:36:05 +0000

    EPBR-9237: Adds journey tests for check your answers page

--- spec/journey/opt_out/name_to_check_your_answers_spec.rb
+++ spec/journey/opt_out/name_to_check_your_answers_spec.rb
@@ -40,7 +40,7 @@ describe "Journey::OptOut::NameToCheckYourAnswers", :journey, type: :feature do
       set_user_login
     end
 
-    context "when inputting Full Name on the /name page" do
+    context "when inputting Full Name on the '/name' page" do
       it "completes the POST and redirects to '/certificate-details' page" do
         visit "#{url}/name"
         fill_in "name", with: "John Test"
@@ -62,7 +62,7 @@ describe "Journey::OptOut::NameToCheckYourAnswers", :journey, type: :feature do
     end
   end
 
-  context "when visiting the /certificate-details page" do
+  context "when visiting the '/certificate-details' page" do
     before do
       visit_login
       set_user_login
@@ -79,6 +79,48 @@ describe "Journey::OptOut::NameToCheckYourAnswers", :journey, type: :feature do
     it "completes the POST and redirects to '/check-your-answers'" do
       expect(page).to have_current_path("/opt-out/check-your-answers")
       expect(page).to have_css("h1", text: "Check your answers before submitting your request")
+    end
+  end
+
+  context "when visiting the '/check-your-answers' page" do
+    before do
+      visit_login
+      set_user_login
+      visit "#{url}/name"
+      fill_in "name", with: "John Test"
+      click_button "Continue"
+      fill_in "certificate_number", with: "1234-1234-1234-1234-1234"
+      fill_in "address-line1", with: "Test Street"
+      fill_in "address-town", with: "London"
+      fill_in "address-postcode", with: "TE5 1NG"
+      click_button "Continue"
+    end
+
+    it "allows you to change your name" do
+      click_link "Change name"
+      fill_in "name", with: "New Tester"
+      click_button "Continue"
+      click_button "Continue"
+      expect(page).to have_css(".govuk-summary-list__row #full-name-value", text: "New Tester")
+    end
+
+    it "allows you to change your relationship to property" do
+      click_link "Change owner"
+      visit_opt_out_occupant
+      find("#label-occupant_yes").click
+      click_button "Continue"
+      set_user_login
+      visit "#{url}/name"
+      click_button "Continue"
+      click_button "Continue"
+      expect(page).to have_css(".govuk-summary-list__row #property-relationship-value", text: "Occupant")
+    end
+
+    it "allows you to change your certificate number" do
+      click_link "Change certificate number"
+      fill_in "certificate_number", with: "2345-2345-2345-2345-2345"
+      click_button "Continue"
+      expect(page).to have_css(".govuk-summary-list__row #certificate-number-value", text: "2345-2345-2345-2345-2345")
     end
   end
 end
