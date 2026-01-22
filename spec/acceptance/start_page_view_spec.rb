624f06c0b942ed41d79cdd1d52548b7438b6b433
Aleksandra Ivanova <aleksandra98iv@gmail.com>
2026-01-22 14:40:03 +0000

    OD-350: Updates start and data options page
    Start Page:
    - Changed the title of the page as per the Mural.
    - Reworded link to the ‘Scottish Register from Energy Performance of Buildings (EPB) Register' to ‘Scottish Energy Performance Certificate Register', and 'Energy Performance of Buildings (EPB) Register’ to 'Energy Performance Certificate Register’.
    - Added a new ‘What will you need’ section.
    
    Data Access Options page:
    - Changed text wording under ‘Download files’
    - Changed button text for ‘Use API’
    - Re-added <br> to fix boxes styling.

--- spec/acceptance/start_page_view_spec.rb
+++ spec/acceptance/start_page_view_spec.rb
@@ -9,7 +9,7 @@ describe "Acceptance::ServiceStartPage", type: :feature do
   describe "get .get-energy-certificate-data.epb-frontend/" do
     context "when the start page is rendered" do
       it "the title to be correct" do
-        expect(response.body).to have_css("h1", text: "Get energy performance of buildings data")
+        expect(response.body).to have_css("h1", text: "Get energy certificate data")
       end
 
       it "has the correct Start now button" do
@@ -17,9 +17,14 @@ describe "Acceptance::ServiceStartPage", type: :feature do
       end
 
       it "has the correct link to Scottish EPB Register, guidance page and EPB Register" do
-        expect(response.body).to have_link("visit the Scottish Energy Performance of Buildings (EPB) Register", href: "https://www.scottishepcregister.org.uk/")
-        expect(response.body).to have_link("visit the Energy Performance of Buildings (EPB) Register", href: "https://www.gov.uk/find-energy-certificate")
+        expect(response.body).to have_link("visit the Scottish Energy Performance Certificate Register", href: "https://www.scottishepcregister.org.uk/")
+        expect(response.body).to have_link("visit the Energy Performance Certificate Register", href: "https://www.gov.uk/find-energy-certificate")
         expect(response.body).to have_link("Visit the guidance page", href: "/guidance")
+      end
+
+      it "has the how you can use and what will you need section" do
+        expect(response.body).to have_css("h2", text: "How you can use this service")
+        expect(response.body).to have_css("h2", text: "What you will need")
       end
 
       it "has the Get Help or Give Feedback section" do
