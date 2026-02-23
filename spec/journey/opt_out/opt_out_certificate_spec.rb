517db0db54882acca8fe14714ea452245e0dc62d
Alice Lui <54395483+al01010@users.noreply.github.com>
2026-02-23 12:28:33 +0000

    EPBR-9237: Refactor Opt Out Journey Tests
    - Remove tests covered by unit tests to speed up the running of the tests

--- spec/journey/opt_out/opt_out_certificate_spec.rb
+++ spec/journey/opt_out/opt_out_certificate_spec.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 require_relative "../../shared_context/shared_opt_out_context"
-require_relative "../../shared_examples/shared_error"
 require_relative "../../shared_context/shared_journey_context"
 
 describe "Journey::OptOut::OptOutCertificate", :journey, type: :feature do
@@ -39,146 +38,39 @@ describe "Journey::OptOut::OptOutCertificate", :journey, type: :feature do
 
   describe "opting out as an owner" do
     before do
-      visit_opt_out_reason
+      visit_login_as_owner
+      set_oauth_cookies
+      visit "#{url}/name"
+      set_name
+      set_certificate_details
     end
 
-    context "when attempting to continue without selecting an reason" do
-      before do
-        click_button "Continue"
-      end
-
-      it_behaves_like "when checking GDS error messages"
-
-      it "remains on the same page" do
-        expect(page).to have_current_path("/opt-out/reason")
-      end
+    it "has the submitted information displayed on the '/check-your-answers' page" do
+      expect(page).to have_current_path("/opt-out/check-your-answers")
+      expect(page).to have_css(".govuk-summary-list__row #property-relationship-value", text: "Owner")
+      expect(page).to have_css(".govuk-summary-list__row #full-name-value", text: "John Test")
+      expect(page).to have_css(".govuk-summary-list__row #certificate-number-value", text: "1234-1234-1234-1234-1234")
+      expect(page).to have_css(".govuk-summary-list__value", text: "Test Street")
+      expect(page).to have_css(".govuk-summary-list__value", text: "London")
+      expect(page).to have_css(".govuk-summary-list__value", text: "TE5 1NG")
     end
 
-    context "when selecting 'other' as the reason for opting out" do
+    context "when submitting the opt-out" do
       before do
-        set_opt_out_reason_other
+        find(".govuk-checkboxes__item #confirmation", visible: :all).click
+        click_button "Submit request"
       end
 
-      it "completes the POST and redirects to the '/owner' page" do
-        expect(page).to have_current_path("/opt-out/owner")
+      it "redirects to the received page" do
+        expect(page).to have_css("h1", text: "Request received")
       end
 
-      context "when on the owner page" do
-        context "when attempting to continue without answering" do
-          before do
-            click_button "Continue"
-          end
-
-          it_behaves_like "when checking GDS error messages"
-
-          it "remains on the same page" do
-            expect(page).to have_current_path("/opt-out/owner")
-          end
-        end
-
-        context "when confirming they are the owner" do
-          before do
-            find("#label-yes").click
-            click_button "Continue"
-          end
-
-          it "completes the POST and redirects to 'login' page" do
-            expect(page).to have_current_path("/login?referer=opt-out")
-          end
-
-          # skip over the login page
-          context "when on the '/name' page" do
-            before do
-              set_oauth_cookies
-              visit "#{url}/name"
-            end
-
-            context "when submitting without inputting a name" do
-              before do
-                click_button "Continue"
-              end
-
-              it_behaves_like "when checking GDS error messages"
-              it "remains on the same page" do
-                expect(page).to have_current_path("/opt-out/name")
-              end
-            end
-
-            context "when inputting a full name in the input" do
-              before do
-                set_name
-              end
-
-              it "completes the POST and redirects to '/certificate-details' page" do
-                expect(page).to have_current_path("/opt-out/certificate-details")
-              end
-
-              context "when on the '/certificate-details' page" do
-                context "when submitting without inputting anything" do
-                  before do
-                    click_button "Continue"
-                  end
-
-                  it_behaves_like "when checking GDS error messages"
-                  it "remains on the same page" do
-                    expect(page).to have_current_path("/opt-out/certificate-details")
-                  end
-                end
-
-                context "when filling out the certificate details page" do
-                  before do
-                    set_certificate_details
-                  end
-
-                  it "completes the POST and redirects to '/check-your-answers'" do
-                    expect(page).to have_current_path("/opt-out/check-your-answers")
-                  end
-
-                  it "has the submitted information displayed on the page" do
-                    expect(page).to have_css(".govuk-summary-list__row #property-relationship-value", text: "Owner")
-                    expect(page).to have_css(".govuk-summary-list__row #full-name-value", text: "John Test")
-                    expect(page).to have_css(".govuk-summary-list__row #certificate-number-value", text: "1234-1234-1234-1234-1234")
-                    expect(page).to have_css(".govuk-summary-list__value", text: "Test Street")
-                    expect(page).to have_css(".govuk-summary-list__value", text: "London")
-                    expect(page).to have_css(".govuk-summary-list__value", text: "TE5 1NG")
-                  end
-
-                  context "when on the '/check-your-answers' page" do
-                    context "when submitting without inputting anything" do
-                      before do
-                        click_button "Submit request"
-                      end
-
-                      it_behaves_like "when checking GDS error messages"
-                      it "remains on the same page" do
-                        expect(page).to have_current_path("/opt-out/check-your-answers")
-                      end
-                    end
-
-                    context "when submitting the opt-out" do
-                      before do
-                        find(".govuk-checkboxes__item #confirmation", visible: :all).click
-                        click_button "Submit request"
-                      end
-
-                      it "redirects to the received page" do
-                        expect(page).to have_css("h1", text: "Request received")
-                      end
-
-                      context "when navigating back after submitting an opt-out" do
-                        it "redirects back to '/opt-out' page" do
-                          expect(page).to have_css("h1", text: "Request received")
-                          page.go_back
-                          click_button "Submit request"
-                          expect(page).to have_css("h1", text: "Opting out an EPC")
-                        end
-                      end
-                    end
-                  end
-                end
-              end
-            end
-          end
+      context "when navigating back after submitting an opt-out" do
+        it "redirects back to '/opt-out' page" do
+          expect(page).to have_css("h1", text: "Request received")
+          page.go_back
+          click_button "Submit request"
+          expect(page).to have_css("h1", text: "Opting out an EPC")
         end
       end
     end
@@ -186,50 +78,24 @@ describe "Journey::OptOut::OptOutCertificate", :journey, type: :feature do
 
   describe "opting out as an occupant" do
     before do
-      visit_opt_out_reason
-      set_opt_out_reason_other
+      visit_opt_out_occupant
+      find("#label-occupant_yes").click
+      click_button "Continue"
+      set_oauth_cookies
+      visit "#{url}/name"
+      set_name
+      set_certificate_details
     end
 
-    context "when confirming they are not the owner" do
-      before do
-        find("#label-no").click
-        click_button "Continue"
-      end
+    it "has the submitted information displayed on the '/check-your-answers' page" do
+      expect(page).to have_current_path("/opt-out/check-your-answers")
+      expect(page).to have_css(".govuk-summary-list__row #property-relationship-value", text: "Occupant")
+    end
 
-      it "completes the POST and redirects to 'occupant' page" do
-        expect(page).to have_current_path("/opt-out/occupant")
-      end
-
-      context "when confirming they are an occupant" do
-        before do
-          find("#label-occupant_yes").click
-          click_button "Continue"
-        end
-
-        it "completes the POST and redirects to 'login' page" do
-          expect(page).to have_current_path("/login?referer=opt-out")
-        end
-
-        context "when they have filled in their name and certificate details" do
-          before do
-            set_oauth_cookies
-            visit "#{url}/name"
-            set_name
-            set_certificate_details
-          end
-
-          it "displays the relationship as occupant on the '/check-your-answers' page" do
-            expect(page).to have_current_path("/opt-out/check-your-answers")
-            expect(page).to have_css(".govuk-summary-list__row #property-relationship-value", text: "Occupant")
-          end
-
-          it "allows them to submit the request" do
-            find(".govuk-checkboxes__item #confirmation", visible: :all).click
-            click_button "Submit request"
-            expect(page).to have_css("h1", text: "Request received")
-          end
-        end
-      end
+    it "allows them to submit the request" do
+      find(".govuk-checkboxes__item #confirmation", visible: :all).click
+      click_button "Submit request"
+      expect(page).to have_css("h1", text: "Request received")
     end
   end
 end
